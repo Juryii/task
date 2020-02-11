62cb2af62a4c6cb3cf6448c9211b890aa45e4f1e
Juryii <polyanskii2118@gmail.com>
2020-02-11 10:42:59 +0600

    rewriting app on components

---
+++ src/components/NewColumn.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="!newColumninput">
+    <button @click="newColumninput = !newColumninput">Добавить новую колонку</button>
+  </div>
+  <div class="addNewColumn" v-else>
+    <input
+      type="text"
+      placeholder="Ввести заголовок списка"
+      v-model="newColumnTitle"
+    ><br>
+    <button @click="addNewColumn">Добавить</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "NewColumn",
+    data: function () {
+      return{
+        newColumninput: false,
+        newColumnTitle: ''
+      }
+    },
+    methods: {
+      addNewColumn() {
+        if(this.newColumnTitle){
+          this.$emit('addNewColumn', this.newColumnTitle);
+          this.newColumnTitle = '';
+          this.newColumninput = false
+        } else{
+          alert('Введите название колонки')
+        }
+
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,98 @@
 <template>
-  <div class="home">
-    <Card/>
+  <div class="home container">
+
+  <div class="column">
+    <Column
+      v-for="(column, index) in columns"
+      :columnTitle="column.title"
+      :columnIndex="index"
+      :key="index"
+      :cardItems="column.cardItems"
+      @editColumnTitle="editColumnTitle"
+      @deleteColumn="deleteColumn"
+      @editElementTitle="editElementTitle"
+      @deleteItemColumn="deleteItemColumn"
+      @addNewCardItem="addNewCardItem"
+    ></Column>
+    <NewColumn
+      @addNewColumn="addNewColumn"
+    ></NewColumn>
+  </div>
+
   </div>
 </template>
 
 <script>
+  import {setObj, getObj} from '@/utils/localStorage';
 // @ is an alias to /src
 import Card from "@/components/Card";
+import Column from "@/components/Column";
+import NewColumn from "@/components/NewColumn";
 
 export default {
   name: 'home',
+  mounted: function () {
+    if (getObj('columns')) {
+      this.columns = getObj('columns');
+    } else {
+      setObj('columns', []);
+    }
+  },
+  data: function () {
+    return {
+      columns: []
+    }
+  },
   components: {
-    Card
+    Column, NewColumn
+  },
+  methods: {
+    editColumnTitle(columnTitle){
+      this.columns[columnTitle.index].title = columnTitle.title;
+      this.updateStorage()
+      },
+    deleteColumn(index){
+      this.columns.splice(index, 1);
+      this.updateStorage()
+    },
+    editElementTitle(title, itemIndex, columnIndex) {
+      this.columns[columnIndex].cardItems[itemIndex].title = title;
+      this.updateStorage()
+    },
+    deleteItemColumn(itemIndex, columnIndex) {
+      this.columns[columnIndex].cardItems.splice(itemIndex, 1);
+      this.updateStorage()
+    },
+    addNewCardItem(args){
+      if(this.columns[args.index].cardItems){
+        this.columns[args.index].cardItems.push({title: args.title});
+      }else {
+        this.columns[args.index].cardItems = [{title: args.title}]
+      }
+      this.updateStorage()
+    },
+    addNewColumn(title){
+      this.columns.push({title: title});
+      this.updateStorage()
+    },
+    updateStorage(){
+      setObj('columns', this.columns)
+    }
   }
 }
 </script>
+<style scoped>
+
+  .container{
+    display: block;
+    flex-direction: row;
+    flex-wrap: nowrap;
+  }
+  .column{
+    display: flex;
+    flex-direction: row;
+    margin-left: 50px;
+    width: 400px;
+  }
+
+</style>
